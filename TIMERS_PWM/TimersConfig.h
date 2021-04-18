c9abdb5dca52479362c855f3a1f4aaab629abfb5
spider <mohamed.el.gallad2015@gmail.com>
2021-04-18 05:14:13 +0200

    documentation refining

--- TIMERS_PWM/TimersConfig.h
+++ TIMERS_PWM/TimersConfig.h
@@ -3,38 +3,59 @@
  *
  *  Created on: 15/03/2021
  *  Author: Mohamed_EL_Gallad
- *  Description:
+ *  Description:This file will contain all the setting required to properly set Timer0 , Timer1 and Timer2 to
+ *  operate as desired
+ *
+ *  TIMERS GENERAL SETTING section is used to define the MCU operating frequency in HZ
+ *
+ *  TIMER0 CONFIGURATIONS section is used to select 1-Timer0's Prescaler
+ *                                                  2-OC0 operating mode
+ *
+ *  TIMER1 CONFIGURATIONS section is used to select 1-Timer1's Prescaler
+ *                                                  2-Phase Correct PWM modes selection
+ *                                                  3-Fast PWM modes selection
+ *                                                  4-CTC modes selection
+ *                                                  5-TIMER1- CHANNEL_A COMPARE MATCH OUTPUT SETTINGS
+ *                                                  6-TIMER1- CHANNEL_B COMPARE MATCH OUTPUT SETTINGS
+ *                                                  7-Input Capture Settings
+ *
+ *  TIMER2 CONFIGURATIONS section is used to select 1-Timer2's Prescaler
+ *                                                  2-OC2 operating mode
  */
 
 #ifndef TIMERSCONFIG_H_
 #define TIMERSCONFIG_H_
 
-#define CPU_FREQ 12000000
-/***************************** TIMER0 & TIMER1 PRESCALER VALUE **********************************************
- * These MACROS define both Timer0 and Timer 1 prscaler values
- * user has to set TIMER0_PRESCALER & TIMER1_PRESCALER to the desired prescaler
- *----------------------------------------------------------------------------------------------------------*/
-#define	NO_PRESCALER        1   // FCPU/1
-#define	CLK_DIV_BY8         2   // FCPU/8
-#define	CLK_DIV_BY64        3   // FCPU/64
-#define	CLK_DIV_BY265       4   // FCPU/265
-#define	CLK_DIV_BY1024      5   // FCPU/1024
-#define	EXT_CLK_FALLING     6   //external clock on T0,Pin B0 ,trigger falling edge
-#define	EXT_CLK_RISSING     7   //external clock on T0,Pin B0 ,trigger rising edge
 
-#define TIMER0_PRESCALER	EXT_CLK_FALLING
-#define TIMER1_PRESCALER    EXT_CLK_FALLING
+/**-----------------------------------------------------------------------------------------------------------*/
+/*                                     TIMERS GENERAL SETTING                                                 */
+/**-----------------------------------------------------------------------------------------------------------*/
+#define CPU_FREQ 12000000  //user has to define the CPU operating frequency
 
-/************************************************************************************************************/
+/**************************************************************************************************************/
 
 
+/**-----------------------------------------------------------------------------------------------------------*/
+/*                                      TIMER0 CONFIGURATIONS                                                 */
+/**-----------------------------------------------------------------------------------------------------------*/
 
-/*************************************** OC0 PIN OPERATION MODE *********************************************
- * these MACROS define the operation modes for the OC0 PIN according the below table
- * user has to set the value of OC0_OPMODE MACRO to the required operation Mode regarding Timer0 functionality
- -----------------------------------------------------------------------------------------------------------*/
+                               /*----------------------------*/
+                               /** TIMER0 PRESCALER SELECT  **/
+                               /*----------------------------*/
+#define	T0_NO_PRESCALER        1   // FCPU/1
+#define	T0_CLK_DIV_BY8         2   // FCPU/8
+#define	T0_CLK_DIV_BY64        3   // FCPU/64
+#define	T0_CLK_DIV_BY265       4   // FCPU/265
+#define	T0_CLK_DIV_BY1024      5   // FCPU/1024
+#define	T0_EXT_CLK_FALLING     6   //external clock on T0,Pin PB0 ,trigger falling edge
+#define	T0_EXT_CLK_RISSING     7   //external clock on T0,Pin PB0 ,trigger rising edge
 
- /*----------------*----------------*----------------*--------------------------*---------------------------*
+#define TIMER0_PRESCALER	T0_NO_PRESCALER  //select your preferred prescaler
+
+                            /*----------------------------------------*/
+                            /** TIMER0 COMPARE MATCH OUTPUT SETTINGS **/
+                            /*----------------------------------------*/
+/*----------------*----------------*----------------*--------------------------*---------------------------*
   | OPERATION      |OC0 MODE0       |OC0 MODE1       |OC0 MODE2                 |OC0 MODE3                  |
   * ---------------*----------------*----------------*--------------------------*---------------------------*
   | NORMAL   MODE  |PIN DICONNECTED |TOGGLE ON MATCH |CLEAR ON MATCH            |SET ON MATCH               |
@@ -53,15 +74,32 @@
 #define OC0_MODE2   2
 #define OC0_MODE3   3
 
-#define OC0_OPMODE OC0_MODE1
-/**************************************************************************************************************/
+#define OC0_OPMODE OC0_MODE2 //select your preferred OC0 operation mode
 
 
+/************************************************************************************************************/
 
-/*
-*in phase and freq correction the update of top value"ICR1 or OCR1A"  will occur when the counter reaches the bottom value
-*while in the other modes their values will be updated when the counter reaches top value
-*/
+
+/**-----------------------------------------------------------------------------------------------------------*/
+/*                                      TIMER1 CONFIGURATIONS                                                      */
+/**-----------------------------------------------------------------------------------------------------------*/
+
+                              /*---------------------------*/
+                              /** TIMER1 PRESCALER SELECT **/
+                              /*---------------------------*/
+#define	T1_NO_PRESCALER        1   // FCPU/1
+#define	T1_CLK_DIV_BY8         2   // FCPU/8
+#define	T1_CLK_DIV_BY64        3   // FCPU/64
+#define	T1_CLK_DIV_BY265       4   // FCPU/265
+#define	T1_CLK_DIV_BY1024      5   // FCPU/1024
+#define	T1_EXT_CLK_FALLING     6   //external clock on T1,Pin PB1 ,trigger falling edge
+#define	T1_EXT_CLK_RISSING     7   //external clock on T1,Pin PB1 ,trigger rising edge
+
+#define TIMER1_PRESCALER    T1_NO_PRESCALER //select your preferred prescaler
+
+                        /*--------------------------------------------------------*/
+                        /** PHASE CORRECT - PHASE AND FREQUENCY CORRECT PWM MODES**/
+                        /*--------------------------------------------------------*/
 /*--------------------------*----------------*
  |         MODE             |  TOP VALUE     |
  * -------------------------*----------------*
@@ -88,10 +126,12 @@
 #define T1_PHASE_FREQ_CORR_MODE1    6
 #define T1_PHASE_FREQ_CORR_MODE2    7
 
-//default phase correct PWM operation mode
-#define TI_PWM_PHASECORR_OPMODE   T1_PHASECORR_MODE5
+//IN CASE PHASE CORRECT MODES ARE NOT USED THE FOLLOWING MACRO SHOULD BE SET TO T1_PHASECORR_DISABLED
+#define TI_PWM_PHASECORR_OPMODE   T1_PHASECORR_DISABLED //select your preferred operation mode
 
-
+                                     /*------------------*/
+                                     /** FAST PWM MODES **/
+                                     /*------------------*/
 /*-------------------*------*---------*
  |         MODE      |  TOP VALUE     |
  * ------------------*----------------*
@@ -112,11 +152,27 @@
 #define T1_FASTPWM_MODE4     4
 #define T1_FASTPWM_MODE5     5
 
-//default fast PWM operation mode
-#define T1_FASTPWM_OPMODE  T1_FASTPWM_DISABLED
+//IN CASE FAST PWM MODES ARE NOT USED THE FOLLOWING MACRO SHOULD BE SET TO T1_FASTPWM_DISABLED
+#define T1_FASTPWM_OPMODE  T1_FASTPWM_DISABLED //select your preferred operation mode
 
-/***********************************COMPARE MATCH MODE SETTING**************************************/
+                                     /*-----------------------*/
+                                     /** COMPARE MATCH MODES **/
+                                     /*-----------------------*/
+/*--------------------*-----------*
+ |        MODE        | TOP VALUE |
+ * -------------------*-----------*
+ | T1_COMPMATCH_MODE1 |  OCR1A    |
+ * -------------------*-----------*
+ | TI_COMPMATCH_MODE2 |  ICR1     |
+ * -------------------*-----------*/
+#define T1_COMPMATCH_MODE1   0
+#define T1_COMPMATCH_MODE2   1
 
+#define T1_COMPMATCH_OPMODE  T1_COMPMATCH_MODE2 //select your preferred operation mode
+
+                            /*---------------------------------------------------*/
+                            /** TIMER1- CHANNEL_A COMPARE MATCH OUTPUT SETTINGS **/
+                            /*---------------------------------------------------*/
  /*----------------*----------------*-------------------------*---------------------------*----------------------------*
   | OPERATION      |OC1A MODE0      |OC1A MODE1               |OC1A MODE2                 |OC1A MODE3                  |
   * ---------------*----------------*-------------------------*---------------------------*----------------------------*
@@ -142,8 +198,11 @@
 #define OC1A_MODE2  2
 #define OC1A_MODE3  3
 
-#define OC1A_OPMODE OC1A_MODE2
+#define OC1A_OPMODE OC1A_MODE2 //select your preferred OC1A operation mode
 
+                            /*---------------------------------------------------*/
+                            /** TIMER1- CHANNEL_B COMPARE MATCH OUTPUT SETTINGS **/
+                            /*---------------------------------------------------*/
  /*----------------*----------------*----------------*--------------------------*----------------------------*
   | OPERATION      |OC1B MODE0      |OC1B MODE1      |OC1B MODE2                |OC1B MODE3                  |
   * ---------------*----------------*----------------*--------------------------*----------------------------*
@@ -161,24 +220,11 @@
 #define OC1B_MODE2  2
 #define OC1B_MODE3  3
 
-#define OC1B_OPMODE OC1B_MODE2
+#define OC1B_OPMODE OC1B_MODE2 //select your preferred OC1A operation mode
 
-/*--------------------*-----------*
- |        MODE        | TOP VALUE |
- * -------------------*-----------*
- | T1_COMPMATCH_MODE1 |  OCR1A    |
- * -------------------*-----------*
- | TI_COMPMATCH_MODE2 |  ICR1     |
- * -------------------*-----------*/
-
-#define T1_COMPMATCH_MODE1   0
-#define T1_COMPMATCH_MODE2   1
-
-#define T1_COMPMATCH_OPMODE  T1_COMPMATCH_MODE2
-
-
-
-/*************************** TIMER_1 INPUT CAPTURE SETTING *****************************************************************************/
+                               /*---------------------------------*/
+                               /** TIMER_1 INPUT CAPTURE SETTING **/
+                               /*---------------------------------*/
 #define ENABLE                   1 // DON'T CHANGE THE DESIGNATED VALUE
 #define DISABLE                  0 // DON'T CHANGE THE DESIGNATED VALUE
 
@@ -192,6 +238,48 @@
 #define NOISE_CANCELLATION  ENABLE
 
 /***************************************************************************************************************************************/
+
+/**-----------------------------------------------------------------------------------------------------------*/
+/*                                      TIMER2 CONFIGURATIONS                                                      */
+/**-----------------------------------------------------------------------------------------------------------*/
+
+                              /*---------------------------*/
+                              /** TIMER2 PRESCALER SELECT **/
+                              /*---------------------------*/
+#define	T2_NO_PRESCALER        1   // FCPU/1
+#define	T2_CLK_DIV_BY8         2   // FCPU/8
+#define	T2_CLK_DIV_BY32        3   // FCPU/32
+#define	T2_CLK_DIV_BY64        4   // FCPU/64
+#define	T2_CLK_DIV_BY128       5   // FCPU/128
+#define	T2_CLK_DIV_BY256       6   // FCPU/256
+#define	T2_CLK_DIV_BY1024      7   // FCPU/1024
+
+#define TIMER2_PRESCALER	T2_NO_PRESCALER //select your preferred prescaler
+
+
+                            /*----------------------------------------*/
+                            /** TIMER0 COMPARE MATCH OUTPUT SETTINGS **/
+                            /*----------------------------------------*/
+ /*----------------*----------------*----------------*--------------------------*---------------------------*
+  | OPERATION      |OC2 MODE0       |OC2 MODE1       |OC2 MODE2                 |OC2 MODE3                  |
+  * ---------------*----------------*----------------*--------------------------*---------------------------*
+  | NORMAL   MODE  |PIN DICONNECTED |TOGGLE ON MATCH |CLEAR ON MATCH            |SET ON MATCH               |
+  * ---------------*----------------*----------------*--------------------------*---------------------------*
+  *                |PIN DICONNECTED |RESERVED        |CLEAR ON MATCH UPCOUNTING |SET ON MATCH UPCOUNTING    |
+  | PHASE CORRECT  |                |                |SET ON MATCH DOWNCOUNTING |CLEAR ON MATCH DOWNCOUNTING|
+  |                |                |                |   NON INVERTED MODE      |      INVERTED MODE        |
+  * ---------------*----------------*----------------*--------------------------*---------------------------*
+  | CTC            |PIN DICONNECTED |TOGGLE ON MATCH |CLEAR ON MATCH            |SET ON MATCH               |
+  * ---------------*----------------*----------------*--------------------------*---------------------------*
+  | FAST PWM       |PIN DICONNECTED |RESERVED        |CLEAR ON MATCH,SET AT TOP |SET ON MATCH,CLEAR AT TOP  |
+  |                |                |                |   NON INVERTED MODE      |       INVERTED MODE       |
+  * ---------------*----------------*----------------*--------------------------*---------------------------*/
+#define OC2_MODE0   0
+#define OC2_MODE1   1
+#define OC2_MODE2   2
+#define OC2_MODE3   3
+
+#define OC2_OPMODE OC2_MODE2 //select your preferred OC2 operation mode
 
 
 #endif /* TIMERSCONFIG_H_ */
